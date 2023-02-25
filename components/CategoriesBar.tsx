654b7cf66ef39e5c27e9df20a9d8fc6dfff8cbc7
Alan Ezequiel Vargas <alanv3533@gmail.com>
2023-02-25 01:23:05 -0300

    feat: brandsView added

---
+++ components/CategoriesBar.tsx
@@ -0,0 +1,23 @@
+import ButtonSection from "./atoms/ButtonSection";
+
+const CategoriesBar = () => {
+  return (
+    <div className="flex flex-col justify-between m-auto h-56 w-9/12 bg-grey text-GRAY pl-10 pr-10 pt-4 pb-4 mb-28 max-sm:w-12/12">
+        <h1 className="text-grey2">!Hagámoslo más personal!</h1>
+        <h3>
+          Selecciona tus intereses para brindarte sugerencia de acuerdo a tus
+          gustos
+        </h3>
+        <div className="flex justify-evenly w-380 max-sm:w-auto">
+          <ButtonSection />
+          <ButtonSection />
+          <ButtonSection />
+        </div>
+        <a href="#" className="text-BLUE font-bold">
+          Ver todos los intereses
+        </a>
+      </div>
+  );
+};
+
+export default CategoriesBar;
