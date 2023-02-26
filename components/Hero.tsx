51bd07fae422c30c54f4404266578a8ec578a778
Jamir Edgar Bances Espinoza <jbances7@gmail.com>
2023-02-26 22:49:04 +0100

    feat: update vista-home

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Input from './atoms/Input';
 
 const Hero = () => {
   return (
@@ -12,39 +13,17 @@ const Hero = () => {
             width={250}
             height={250}
           />
-          <div className="relative">
-            <input
-              className="h-11 w-96 md:w-96 pl-4 pr-12 rounded-3xl mt-7 text-black"
-              type="search"
-              name=""
-              id=""
-              placeholder="¿Qué quieres ver en tu ciudad?"
-            />
-            <div className="absolute bottom-[12%] right-[5%]  flex items-center pl-3 text-gray-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </div>
+          <div className='mt-5 text-black'>
+            <Input />
           </div>
-          <div className='flex flex-row justify-between w-96 pt-5 text-sm'>
-            <button className='bg-white text-gray-500 border-gray-500 h-7 rounded-2xl px-4'>
+          <div className="flex flex-row justify-between w-96 pt-5 text-sm">
+            <button className="bg-white text-gray-500 border-gray-500 h-7 rounded-2xl px-4">
               Marcas y tiendas
             </button>
-            <button className='bg-white text-gray-500 border-gray-500 h-7 rounded-2xl px-4'>
+            <button className="bg-white text-gray-500 border-gray-500 h-7 rounded-2xl px-4">
               Artistas y conciertos
             </button>
-            <button className='bg-white text-gray-500 border-gray-500 h-7 rounded-2xl px-4'>
+            <button className="bg-white text-gray-500 border-gray-500 h-7 rounded-2xl px-4">
               Torneos
             </button>
           </div>
