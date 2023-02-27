5c9e535a75f648d388cdc1ad9a9ecf3694b260ec
Alan Ezequiel Vargas <alanv3533@gmail.com>
2023-02-26 22:19:58 -0300

    feat: login section added and fixed

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,58 +1,51 @@
-import { useState } from 'react';
+import LogoClose from "./svgs/IconClose";
 
 const Login = () => {
-  const [show, setShow] = useState(true);
-
   return (
     <>
-      {show && (
-        <div className="h-483px w-362px bg-zinc-800/90 text-white rounded-2xl p-5 grid grid-rows-auto gap-y-2 shadow-2xl absolute top-20 right-20 z-10">
-          <div className="h-5 flex justify-end">
-            <span
-              className="text-center rounded-full w-8 h-8 text-yellow-300 font-bold cursor-pointer"
-              onClick={() => setShow(!show)}
-            >
-              x
-            </span>
-          </div>
-
-          <div className="">
-            <h1>¡Hola!</h1>
-            <p>Inicie sesión con los datos que ingresó durante su registro.</p>
-          </div>
-
-          <div>
-            <form className="flex-row">
-              <legend>Email</legend>
-              <input
-                className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 p-4 placeholder:text-white"
-                type="text"
-                placeholder="john.doe@gmail.com"
-              />
-              <legend>Contraseña</legend>
-              <input
-                className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
-                type="password"
-                placeholder="***********"
-              />
-            </form>
-            <p className="text-center">
-              ¿Olvidaste tu contraseña?{' '}
-              <a className="text-yellow-300 underline" href="#">
-                Recupérala Aquí
-              </a>
-            </p>
-          </div>
-          <div className="flex flex-col w-full m-auto">
-            <button className="bg-yellow-300 text-BLACKLIGHT font-bold rounded-md h-10">
-              Iniciar sesión
-            </button>
-            <a className="text-yellow-300 underline m-auto" href="#">
-              O crear una cuenta
-            </a>
-          </div>
+      <div className="w-378 h-529 border bg-zinc-800/90 text-white rounded-2xl p-8 grid grid-rows-auto gap-y-2 shadow-2xl z-10 relative lg:w-557 lg:h-560 lg:pl-16 lg:pr-16" >
+        
+        <div className="h-10 w-h-10 absolute right-5 top-5">
+          <LogoClose/>
         </div>
-      )}
+        
+
+        <div className="flex flex-col justify-end">
+          <h1>¡Hola!</h1>
+          <p>Inicie sesión con los datos que ingresó durante su registro.</p>
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <form className="flex-row">
+            <legend>Email</legend>
+            <input
+              className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 p-4 placeholder:text-white"
+              type="text"
+              placeholder="john.doe@gmail.com"
+            />
+            <legend>Contraseña</legend>
+            <input
+              className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
+              type="password"
+              placeholder="***********"
+            />
+          </form>
+          <p className="text-center">
+            ¿Olvidaste tu contraseña?{' '}
+            <a className="text-yellow-300 underline" href="#">
+              Recupérala Aquí
+            </a>
+          </p>
+        </div>
+        <div className="flex flex-col w-full m-auto">
+          <button className="bg-yellow-300 text-BLACKLIGHT font-bold rounded-md h-10">
+            Iniciar sesión
+          </button>
+          <a className="text-yellow-300 underline m-auto" href="#">
+            O crear una cuenta
+          </a>
+        </div>
+      </div>
     </>
   );
 };
--- components/SingIn.tsx
+++ components/SingIn.tsx
@@ -1,71 +1,61 @@
-import { useState } from 'react';
+import LogoClose from "./svgs/IconClose";
 
 const SignIn = () => {
-  const [show, setShow] = useState(true);
-
   return (
     <>
-      {show && (
-        <div className="h-483px w-362px bg-zinc-800/90 text-white rounded-2xl p-5 grid grid-rows-auto gap-y-2 shadow-2xl absolute top-20 right-20 z-10">
-          <div className="h-5 flex justify-end">
-            <span
-              className="text-center rounded-full w-8 h-8 text-yellow-300 font-bold cursor-pointer"
-              onClick={() => setShow(!show)}
-            >
-              x
-            </span>
-          </div>
-
-          <div className="">
-            <h1>Todos Votamos :)</h1>
-            <p>Regístrate para ingresar.</p>
-          </div>
-
-          <div>
-            <form className="flex-row">
-              <legend>Email</legend>
-              <input
-                className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 p-4 placeholder:text-white"
-                type="text"
-                placeholder="john.doe@gmail.com"
-              />
-              <div className="flex flex-row gap-x-3">
-                <div>
-                  <legend>Nombre</legend>
-                  <input
-                    className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
-                    type="password"
-                    placeholder="John"
-                  />
-                </div>
-                <div>
-                  <legend>Apellido</legend>
-                  <input
-                    className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
-                    type="password"
-                    placeholder="Doe"
-                  />
-                </div>
-              </div>
-              <legend>Contraseña</legend>
-              <input
-                className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
-                type="password"
-                placeholder="***********"
-              />
-            </form>
-            
-          </div>
-          <div className="flex flex-col w-full m-auto">
-            <button className="bg-yellow-300 text-BLACKLIGHT font-bold rounded-md h-10">
-              Crear cuenta
-            </button>
-            <a className="text-yellow-300 underline m-auto" href="#">
-              O inicia sesión
-            </a>
-          </div>
+      <div className="w-378 h-529 p-8 border bg-zinc-800/90 text-white rounded-2xl pl-16 pr-16 grid grid-rows-auto gap-y-2 shadow-2xl z-10 relative lg:w-557 lg:h-560 lg:p-8">
+        <div className="h-32 w-32 absolute -right-16 top-5">
+          <LogoClose/>
         </div>
-      )}
+
+        <div className="flex flex-col justify-center">
+          <h1>Todos Votamos :)</h1>
+          <p>Regístrate para ingresar.</p>
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <form className="flex-row">
+            <legend>Email</legend>
+            <input
+              className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 p-4 placeholder:text-white"
+              type="text"
+              placeholder="john.doe@gmail.com"
+            />
+            <div className="flex flex-row justify-between">
+              <div>
+                <legend>Nombre</legend>
+                <input
+                  className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
+                  type="password"
+                  placeholder="John"
+                />
+              </div>
+              <div>
+                <legend>Apellido</legend>
+                <input
+                  className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
+                  type="password"
+                  placeholder="Doe"
+                />
+              </div>
+            </div>
+            <legend>Contraseña</legend>
+            <input
+              className="w-full rounded-md bg-zinc-800/40 border-r-2 border h-12 pl-4 placeholder:text-white"
+              type="password"
+              placeholder="***********"
+            />
+          </form>
+        </div>
+        <div className="flex flex-col w-full m-auto">
+          <button className="bg-yellow-300 text-BLACKLIGHT font-bold rounded-md h-10">
+            Crear cuenta
+          </button>
+          <a className="text-yellow-300 underline m-auto" href="#">
+            O inicia sesión
+          </a>
+        </div>
+      </div>
     </>
   );
 };
