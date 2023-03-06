c4796e7d7ea6aa528cb46376876ae9b20443612b
Alan Ezequiel Vargas <alanv3533@gmail.com>
2023-03-06 13:39:54 -0300

    feat: logo svg with props added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import ButtonSection from './atoms/ButtonSection';
 import Input from './atoms/Input';
+import LogoParaCuando from './svgs/LogoParaCuando';
 
 const Hero = () => {
   const router = useRouter();
@@ -10,24 +10,24 @@ const Hero = () => {
     <div className="relative h-[30.5rem] bg-[url('/assets/hero-background.jpg')] bg-cover bg-center">
       <div className="absolute w-full h-full ">
         <div className="flex justify-center items-center flex-col h-full gap-y-5">
-          <Image
-            src={'/assets/para-cuando-logo.png'}
-            alt={'para-cuando-logo'}
-            width={250}
-            height={250}
-          />
+          <div className="w-52">
+            <LogoParaCuando color="#FFFFFF" />
+          </div>
           <div className="relative w-[343px] sm:w-[465px]">
-            <Input/>
+            <Input />
           </div>
           <div className="flex flex-row justify-around text-sm w-[362px] sm:w-[400px]">
             <div onClick={() => router.push('/brands')}>
-              <ButtonSection name="Marcas y tiendas" style='h-8 px-1 sm:px-3'/>
+              <ButtonSection name="Marcas y tiendas" style="h-8 px-1 sm:px-3" />
             </div>
             <div onClick={() => router.push('/artists')}>
-              <ButtonSection name="Artistas y conciertos" style='h-8 px-1 sm:px-3'/>
+              <ButtonSection
+                name="Artistas y conciertos"
+                style="h-8 px-1 sm:px-3"
+              />
             </div>
             <div onClick={() => router.push('/tournaments')}>
-              <ButtonSection name="Torneos" style='h-8 px-1 sm:px-3'/>
+              <ButtonSection name="Torneos" style="h-8 px-1 sm:px-3" />
             </div>
           </div>
         </div>
