3f4cb19ab897a1319d0b7b3eae739243380a190a
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-03-12 23:07:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   publicRuntimeConfig: {
     BASE_URL:
       process.env.NODE_ENV === 'development'
-        ? 'https://paracuando-gen19-team-2.academlo.tech/api/v1'
-        : 'https://paracuando-gen19-team-2.academlo.tech/api/v1',
+        ? 'https://paracuando-gen19-team-9.academlo.tech/api/v1'
+        : 'https://paracuando-gen19-team-9.academlo.tech/api/v1',
   },
   reactStrictMode: true,
   /* swcMinify: true,
