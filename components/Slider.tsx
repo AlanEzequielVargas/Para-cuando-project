cdc9852270e196c35ad697eb577fcf04ad95a027
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-03-16 16:33:34 -0300

    Update Slider.tsx

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -9,75 +9,76 @@ import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import Card from './Card';
 import ButtonNext from './svgs/ButtonNext';
 
-interface ISliderProps {
+interface ISliderProps{
   title: string;
   subtitle: string;
 }
 
-const Slider: React.FC<ISliderProps> = ({ title, subtitle }: ISliderProps) => {
+const Slider: React.FC<ISliderProps> = ({title,subtitle}:ISliderProps) => {
   const [hidden, setHidden] = useState('hidden');
   const [hiddenTwo, setHiddenTwo] = useState('');
   const { data, error, isLoading } = usePublications();
-
+  
   const router = useRouter();
 
-  const { mutate } = usePublications();
+  const {mutate} = usePublications();
 
   return (
     <div className="w-full m-auto md:w-10/12 lg:w-[1000px] xl:w-[970px] mt-10 font-roboto">
-      {isLoading ? (
-        <div className="text-4xl w-full h-screen m-auto">Cargando...</div>
-      ) : (
-        <div>
-          <div className="relative">
-            <div className="w-full max-md:px-12 py-10">
-              <h1 className="font-bold">{title}</h1>
-              <h3 className="text-[#6E6A6C] text-[19px]">{subtitle}</h3>
-            </div>
-            <Swiper
-              className="h-550 mb-12"
-              // install Swiper modules
-              modules={[]}
-              spaceBetween={0}
-              slidesPerView={3}
-              breakpoints={{
-                0: {
-                  slidesPerView: 1.2,
-                },
-                436: {
-                  slidesPerView: 1.4,
-                },
-                600: {
-                  slidesPerView: 2,
-                },
-                730: {
-                  slidesPerView: 2.2,
-                },
-                900: {
-                  slidesPerView: 2,
-                },
-                1200: {
-                  slidesPerView: 3,
-                },
-              }}
-              /* pagination={{ clickable: true }}
+      {isLoading ? (<div className='text-4xl w-full h-screen m-auto'>Cargando...</div>) : (
+      <div>  
+        <div className="relative">
+        <div className="w-full max-md:px-12 py-10">
+          <h1 className="font-bold">{title}</h1>
+          <h3 className="text-[#6E6A6C] text-[19px]">{subtitle}</h3>
+        </div>
+          <Swiper
+            className="h-550 mb-12"
+            // install Swiper modules
+            modules={[]}
+            spaceBetween={0}
+            slidesPerView={3}
+            breakpoints={{
+              0: {
+                slidesPerView: 1.2,
+              },
+              436: {
+                slidesPerView: 1.4,
+              },
+              600: {
+                slidesPerView: 2,
+              },
+              730: {
+                slidesPerView: 2.2,
+              },
+              900: {
+                slidesPerView: 2,
+              },
+              1200: {
+                slidesPerView: 3,
+              },
+            }}
+            /* pagination={{ clickable: true }}
           scrollbar={{ draggable: true }} */
-              /* onSwiper={(swiper) => console.log(swiper)} */
-              onSlideChange={(swiper) => {
-                if (swiper.isEnd) {
-                  setHidden('');
-                  setHiddenTwo('hidden');
-                } else if (swiper.isBeginning) {
-                  setHidden('hidden');
-                  setHiddenTwo('');
-                } else if (!swiper.isBeginning && !swiper.isEnd) {
-                  setHidden('');
-                  setHiddenTwo('');
-                }
-              }}
-              style={{ position: 'unset' }}
+            /* onSwiper={(swiper) => console.log(swiper)} */
+            onSlideChange={(swiper) => {
+              if (swiper.isEnd) {
+                setHidden('');
+                setHiddenTwo('hidden');
+              } else if (swiper.isBeginning) {
+                setHidden('hidden');
+                setHiddenTwo('');
+              } else if (!swiper.isBeginning && !swiper.isEnd) {
+                setHidden('');
+                setHiddenTwo('');
+              }
+            }}
+            style={{ position: 'unset' }}
+          >
+            <div
+              id="arrow"
+              className={`cursor-pointer absolute md:-left-14 lg:-left-20 top-[388px] z-30 transform -scale-x-100 max-md:hidden ${hidden}`}
             >
-
               <PrevButton />
             </div>
             {data?.results.results.map(
@@ -109,17 +110,16 @@ const Slider: React.FC<ISliderProps> = ({ title, subtitle }: ISliderProps) => {
               )
             )}
 
-
-              <div
-                id="arrow"
-                className={`cursor-pointer absolute md:-right-12 lg:-right-20 top-[388px] z-30 max-md:hidden ${hiddenTwo}`}
-              >
-                <NextButton />
-              </div>
-            </Swiper>
-          </div>
+            <div
+              id="arrow"
+              className={`cursor-pointer absolute md:-right-12 lg:-right-20 top-[388px] z-30 max-md:hidden ${hiddenTwo}`}
+            >
+              <NextButton />
+            </div>
+          </Swiper>
         </div>
-      )}
+      </div>)}
+      
     </div>
   );
 };
@@ -135,3 +135,4 @@ const PrevButton = () => {
 };
 
 export default Slider;
+
